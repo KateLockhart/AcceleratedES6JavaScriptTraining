15d543af96c09e5876b90061e18c415f0fbe960e
KateLockhart <katelockhart707@gmail.com>
2022-06-24 22:07:53 -0400

    Promise chaining portion complete.

--- 05_Promises/script.js
+++ 05_Promises/script.js
@@ -1,5 +1,31 @@
 /*
     Promises - helper object to help with asynchronous tasks, much more simple than nested callbacks thanks to promise chaining 
+    - Resolve for success case, reject for error case
+
 */
 
-let promise = new Promise(() => {});
+// let promise = new Promise(function(resolve, reject) {
+//     setTimeout(function() {
+//         resolve('Done!');
+//     }, 1500);
+// });
+
+let promise = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    reject("Failed!");
+  }, 1500);
+});
+
+// Handles the data received with next versus having nesting callbacks
+promise.then(
+  function (value) {
+    console.log(value);
+  },
+  function (error) {
+    console.log(error);
+  }
+);
+
+/*
+    Chaining Promises
+*/
