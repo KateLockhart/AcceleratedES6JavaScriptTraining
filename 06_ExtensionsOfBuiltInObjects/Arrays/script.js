23673ce3bcecae42325b96a5957e9bb46a9b7ef4
KateLockhart <katelockhart707@gmail.com>
2022-07-02 18:54:34 -0400

    Finished array section of Extensions of Built-in Objects.

--- 06_ExtensionsOfBuiltInObjects/Arrays/script.js
+++ 06_ExtensionsOfBuiltInObjects/Arrays/script.js
@@ -4,6 +4,12 @@
     - .from method: allows to specify which array to construct an array from, the two arguments expected are (theArrayToReference, aFunctionToPerformSomeAugmentation/Change); original array is not changed as a new array is made
     - .fill method: applied directly to an array, each value of original array is replaced with the value passed to fill, it "fills" the array with only that value; can specify which index to fill by passing additional arguments. Does change the original array.
     - .find method: expects a function as an input with criteria for array values to pass, will only result with first value that passes and results in true (only one value returned). Good for searching through large arrays to find a specific value.
+    - .copyWithin method: allows us to copy values from inside an array with arguments of ( targetIndexToBeChanged, targetIndexWhoseValueWeWantToCopy, endIndexOfPlaceToCopy(optional) ); does mutate/change the original array
+    - .entries method: array iterator, can be used in loop functionality to provide the indexes and values of an array in array pairs of [ indexOfValueInTheArray, theValueAtThatIndex ]
+*/
+
+/*
+    Methods that are not built off the global Array object, but are built off of the variable of an array value will alter the original array and not create a new one.
 */
 
 // Can use the array object as a constructor with .of() to specify values
@@ -39,3 +45,30 @@ function findCherries(fruit) {
 }
 // Helpful for making advanced queries into an array of objects
 console.log(inventory.find(findCherries)); // { name: 'cherries', quantity: 5 }
+
+// .copyWithin()
+let anotherArray = [1, 2, 3];
+console.log(anotherArray.copyWithin(1, 2)); // [ 1, 3, 3 ]
+// CopyWithin does change the original array
+console.log(anotherArray); // [ 1, 3, 3 ]
+console.log(anotherArray.copyWithin(1, 0)); // [ 1, 1, 3 ]
+
+let anotherArrayEx2 = [1, 2, 3];
+// Third argument of index to copy to
+console.log(anotherArrayEx2.copyWithin(1, 0, 2)); // [ 1, 1, 2 ]
+
+// .entries()
+let exArray = [1, 2, 3];
+
+console.log(exArray.entries()); // Object [Array Iterator] {}
+
+let it = exArray.entries();
+/* Result in console:
+    [ 0, 1 ]
+    [ 1, 2 ]
+    [ 2, 3 ]
+*/
+// [ indexOfValueInTheArray, theValueAtThatIndex ]
+for (let element of it) {
+  console.log(element);
+}
