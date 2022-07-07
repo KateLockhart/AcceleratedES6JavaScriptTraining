a3cb9b04b654b592257ad84b5c5a42b1f45165fd
KateLockhart <katelockhart707@gmail.com>
2022-07-07 14:14:14 -0400

    Implimented http.js and weatherData.js exports and logic in script.js to build fetch functionality.

--- 10_CourseProject/script.js
+++ 10_CourseProject/script.js
@@ -1,5 +1,6 @@
 import * as ELEMENTS from "./elements.js";
 import { Http } from "./http.js";
+import { WeatherData, WEATHER_PROXY_HANDLER } from "./weatherData.js";
 
 const APP_ID = "857650671a3dbc8cc194679ccd753990";
 
@@ -14,6 +15,13 @@ function searchWeather() {
   const URL = `http://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&units=metric&appid=${APP_ID}`;
 
   Http.fetchData(URL)
-    .then((responseData) => {})
+    .then((responseData) => {
+      const WEATHER_DATA = new WeatherData(
+        CITY_NAME,
+        responseData.weather[0].description.toUpperCase()
+      );
+      const WEATHER_PROXY = new Proxy(WEATHER_DATA, WEATHER_PROXY_HANDLER);
+      WEATHER_PROXY.temperature = responseData.main.temp;
+    })
     .catch((error) => alert(error));
 }
