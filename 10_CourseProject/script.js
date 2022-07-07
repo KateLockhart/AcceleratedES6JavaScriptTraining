4f3bf77c0735af0977e7d962369d26eb0267670f
KateLockhart <katelockhart707@gmail.com>
2022-07-07 13:05:22 -0400

    Created http.js to house exported HTTP and promise logic, added eventListner to button in script.js, updated script tag in HTML body to allow for module use of import and export.

---
+++ 10_CourseProject/script.js
@@ -0,0 +1,11 @@
+import * as ELEMENTS from "./elements.js";
+
+ELEMENTS.ELEMENT_SEARCH_BUTTON.addEventListener("click", searchWeather);
+
+function searchWeather() {
+  const CITY_NAME = ELEMENTS.ELEMENT_SEARCHED_CITY.value.trim();
+  if (CITY_NAME.length == 0) {
+    return alert("Please enter a city name.");
+  }
+  alert(CITY_NAME);
+}
