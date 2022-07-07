9d1cc2d1baa462dd0de706a8591979d9ef9be842
KateLockhart <katelockhart707@gmail.com>
2022-07-07 14:35:59 -0400

    Finshed adding fetch results to DOM, course project complete and functioning.

--- 10_CourseProject/script.js
+++ 10_CourseProject/script.js
@@ -12,7 +12,10 @@ function searchWeather() {
     return alert("Please enter a city name.");
   }
 
-  const URL = `http://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&units=metric&appid=${APP_ID}`;
+  ELEMENTS.ELEMENT_WEATHER_BOX.style.display = "none";
+  ELEMENTS.ELEMENT_LOADING_TEXT.style.display = "block";
+
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&units=metric&appid=${APP_ID}`;
 
   Http.fetchData(URL)
     .then((responseData) => {
@@ -20,8 +23,19 @@ function searchWeather() {
         CITY_NAME,
         responseData.weather[0].description.toUpperCase()
       );
+      console.log(WEATHER_DATA);
       const WEATHER_PROXY = new Proxy(WEATHER_DATA, WEATHER_PROXY_HANDLER);
       WEATHER_PROXY.temperature = responseData.main.temp;
+      updateWeather(WEATHER_PROXY);
     })
     .catch((error) => alert(error));
+}
+
+function updateWeather(weatherData) {
+  ELEMENTS.ELEMENT_WEATHER_CITY.textContent = weatherData.cityName;
+  ELEMENTS.ELEMENT_WEATHER_DESCRIPTION.textContent = weatherData.description;
+  ELEMENTS.ELEMENT_WEATHER_TEMPERATURE.textContent = weatherData.temperature;
+
+  ELEMENTS.ELEMENT_LOADING_TEXT.style.display = "none";
+  ELEMENTS.ELEMENT_WEATHER_BOX.style.display = "block";
 }
